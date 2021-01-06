7591e0e3adab05267d153e1eda112abd6d2b1847
dave.lee <sirdavidoflee@gmail.com>
2021-01-06 16:11:00 -0500

    updated packages

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: 'us-east-1' env('AWS_BUCKET_REGION'),
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_ACCESS_SECRET,
+      region: process.env.AWS_BUCKET_REGION,
       params: {
-        Bucket: 'cdn.probablybad.com' env('AWS_BUCKET_NAME'),
+        Bucket: process.env.AWS_BUCKET_NAME,
       },
     }
   }
